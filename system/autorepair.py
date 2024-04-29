f7869d216c2bfda2eb4273f431a6d0e5b3dd3b96
Extraskilled56 <zjones.092912@gmail.com>
2024-04-29 18:13:27 -0500

    update 1.6.0 final patch

--- system/autorepair.py
+++ system/autorepair.py
@@ -3,41 +3,21 @@ import os
 
 def repair(item):
     if item == "PYmath":
-        if os.path.isfile("system/sys_software/PYmath/") == True:
-            os.system("cd system/sys_software/PYmath")
-            os.system("curl -o PYmath.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYmath/PYmath.py")
-        else:
-            os.system("cd system/sys_software")
-            os.system("mkdir PYmath")
-            os.system("curl -o PYmath.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYmath/PYmath.py")
+        os.system("curl -o system/sys_software/PYmath/PYmath.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYmath/PYmath.py")
         print("attempted to repaired PYmath, please re-run CmdOS to chech")
     elif item == "PYpaint":
-        if os.path.isfile("system/sys_software/PYpaint/") == True:
-            os.system("cd system/sys_software/PYpaint")
-            os.system("curl -o PYpaint.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYpaint/PYpaint.py")
-        else:
-            os.system("cd system/sys_software")
-            os.system("mkdir PYpaint")
-            os.system("curl -o PYpaint.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYpaint/PYpaint.py")
+        os.system("curl -o system/sys_software/PYpaint/PYpaint.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYpaint/PYpaint.py")
         print("attempted to repaired PYpaint, please re-run CmdOS to chech")
         
     elif item == "PYpad":
-        if os.path.isfile("system/sys_software/PYpad/") == True:
-            os.system("cd system/sys_software/PYpad")
-            os.system("curl -o PYpad.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYpad/PYpad.py")
-        else:
-            os.system("cd system/sys_software")
-            os.system("mkdir PYpad")
-            os.system("curl -o PYpad.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYpad/PYpad.py")
+        os.system("curl -o system/sys_software/PYpad/PYpad.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/system/sys_software/PYpad/PYpad.py")
         print("attempted to repaired PYpaint, please re-run CmdOS to chech")
-    #TODO Finish doing this with code from autorepair2.py 
     elif item == "poss1":
-        os.system("cd software/Poss")
-        os.system("curl -o pcommands.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/software/Poss/pcommands.py")
+        os.system("curl -o Poss/pcommands.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/software/Poss/pcommands.py")
         print("attempted to repaired poss, please re-run CmdOS to chech")
     elif item == "poss2":
-        os.system("cd software/Poss")
-        os.system("curl -o posspmanager.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/software/Poss/posspmanager.py")
+        os.system("curl -o Poss/posspmanager.py https://git.bitsyncdev.com/bit-sync/cmdos/raw/branch/main/software/Poss/posspmanager.py")
         print("attempted to repaired poss, please re-run CmdOS to chech")
     else:
         print(item + " not compatable with auto repair")
+        
